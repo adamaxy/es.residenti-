5180d7a6ba38ec5a65b86e27f910ce36ed71e84e
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-08-02 14:33:56 +0200

    Update HouseTester.java

--- HouseTester.java
+++ HouseTester.java
@@ -1,38 +1,5 @@
 import java.util.Scanner;
 
-class House {
-    private int floorsNumber;
-    private String address;
-    private String[] residentsNames;
-
-    // Getter e Setter per floorsNumber
-    public int getFloorsNumber() {
-        return floorsNumber;
-    }
-
-    public void setFloorsNumber(int floorsNumber) {
-        this.floorsNumber = floorsNumber;
-    }
-
-    // Getter e Setter per address
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    // Getter e Setter per residentsNames
-    public String[] getResidentsNames() {
-        return residentsNames;
-    }
-
-    public void setResidentsNames(String[] residentsNames) {
-        this.residentsNames = residentsNames;
-    }
-}
-
 public class HouseTester {
     public static void main(String[] args) {
         House house = new House();
