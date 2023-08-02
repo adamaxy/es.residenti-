3c24ccbae213193653be3ad01c8ddd592d211fba
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-08-02 14:34:33 +0200

    Create House.java

---
+++ House.java
@@ -0,0 +1,32 @@
+class House {
+    private int floorsNumber;
+    private String address;
+    private String[] residentsNames;
+
+    // Getter e Setter per floorsNumber
+    public int getFloorsNumber() {
+        return floorsNumber;
+    }
+
+    public void setFloorsNumber(int floorsNumber) {
+        this.floorsNumber = floorsNumber;
+    }
+
+    // Getter e Setter per address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Getter e Setter per residentsNames
+    public String[] getResidentsNames() {
+        return residentsNames;
+    }
+
+    public void setResidentsNames(String[] residentsNames) {
+        this.residentsNames = residentsNames;
+    }
+}
